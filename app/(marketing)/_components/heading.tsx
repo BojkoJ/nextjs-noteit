c0801c2d89021649e7affdbe8650110d490f7f27
Jan Bojko <honzabojko@seznam.cz>
2023-10-25 16:03:40 +0200

    Documents sidebar FE, convex realtime DB setup + working.

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import Spinner from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <div className='max-w-3xl space-y-4'>
       <h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -16,10 +22,27 @@ const Heading = () => {
         better, faster work happens.
       </h3>
 
-      <Button>
-        Enter Jotion
-        <ArrowRight className='h-4 w-4 ml-2' />
-      </Button>
+      {isLoading && (
+        <div className='w-full flex items-center justify-center'>
+          <Spinner size='lg' />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href='/documents'>
+            Enter Jotion <ArrowRight className='h-4 w-4 ml-2' />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode='modal'>
+          <Button>
+            Get Jotion Free <ArrowRight className='h-4 w-4 ml-2' />
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://genuine-kiwi-96.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
